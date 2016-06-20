0301d54085578e5bcc782867931c873d48506de6
ostea <cnostea@iCloud.com>
2016-06-20 19:15:55 +0800

    commit20160620

---
+++ Day160620.playground/Contents.swift
@@ -0,0 +1,33 @@
+import UIKit
+
+let app=3
+
+let appName="i have a \(app)"
+
+var shopLists=["item1","item2"]
+shopLists[0]
+var occDir=[
+        "name":"thorn",
+        "age":24,
+        "sex":0
+]
+let empytArray=[String]()
+let emptyDic=[String:Float]()
+
+occDir["name"]
+occDir.count
+occDir["age"]
+
+//2.控制流
+let scores=[66,56,38,95,68]
+var teamScore=0
+for temp in scores{
+    
+    if temp > 60 {
+        teamScore+=temp
+    }
+    
+}
+teamScore
+
+//nil or
